b4b8389e17624118d8204f06ac4c4e081197d236
ASHWANEETH <ashwaneeth@gmail.com>
2024-12-20 23:45:26 +0530

    ..

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://rollexjhony:Ashwa1234@cluster0.rk45z.mongodb.net/orderManage?retryWrites=true&w=majority&appName=Cluster0", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect("mongodb+srv://rollexjhony:Ashwa1234@cluster0.rk45z.mongodb.net/orderManage?retryWrites=true&w=majority&appName=Cluster0");
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection error:", err);
