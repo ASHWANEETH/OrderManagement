ab1f653a5e1d96414ec4829232b0851031d88909
ASHWANEETH <ashwaneeth@gmail.com>
2024-12-22 15:34:01 +0530

    ..

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -209,14 +209,15 @@ const handlePayment = async (table) => {
     <div style={{ fontFamily: "Arial, sans-serif", padding: "20px", backgroundColor: "#f4f4f4" }}>
       {/* Notifications */}
       {notifications.length > 0 && (
-        <div
+        <>
+        <span
           style={{
             // position: "absolute",
             top: "20px",
             left: "50%",
             // transform: "translateX(-50%)",
             backgroundColor: "#FFB74D", // Light Orange
-            color: "white",
+            color: "black",
             padding: "10px 20px",
             borderRadius: "5px",
             boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
@@ -225,10 +226,12 @@ const handlePayment = async (table) => {
           }}
         >
           {notifications[notifications.length - 1]}
-        </div>
+        </span>
+        <br />
+        </>
       )}
 
-      <h1 style={{ textAlign: "center", color: "#3F51B5", fontWeight: "bold" }}>Jai Bhavani Savaji Hotel
+      <h1 style={{ textAlign: "center", color: "#3F51B5", fontWeight: "bold" }}>Jai Bhavani Savaji Hotel.
       </h1>
 
       {/* Role Selection */}
