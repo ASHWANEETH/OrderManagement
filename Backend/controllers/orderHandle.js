5853490f7f478bb6d2abc5f9a506f55f70b04c27
ASHWANEETH <ashwaneeth@gmail.com>
2024-12-23 01:20:16 +0530

    ..

--- Backend/controllers/orderHandle.js
+++ Backend/controllers/orderHandle.js
@@ -50,6 +50,10 @@ async function handleRemItem(req, res) {
       return res.status(404).send("Table not found.");
     }
 
+    if (updatedOrder.orders.length === 0) {
+      await TableOrder.deleteOne({ tableId: tableId }); // Delete the table if no orders are left
+      return res.status(200).send(`Table ${tableId} deleted as no orders remain.`);
+    }
     // Respond with the updated order
     res.status(200).json(updatedOrder.orders);
   } catch (err) {
